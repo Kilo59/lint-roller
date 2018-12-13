3a078d146ce0a6464a553c87dac077010f2ad534
Gabriel <gabriel59kg@gmail.com>
2018-12-13 00:24:58 -0500

    update conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import pathlib
 from typing import Dict, Optional
 import pytest
 
-TEST_ROOT = pathlib.Path(__file__).joinpath("..")
+TEST_ROOT = pathlib.Path(__file__).joinpath("..").resolve()
 TEST_DOCS = TEST_ROOT.joinpath("docs")
 
 
@@ -13,13 +13,15 @@ def package_maker(pkg_name: str, pkg_content: Optional[Dict] = None):
             code_content = f_in.read()
         pkg_content = {"__main__.py": code_content}
 
-    init_path = pathlib.Path(f"{pkg_name}/__init__.py")
-    with open(init_path) as f_out:
-        f_out.write(f"# {pkg_name} __init__ file")
+    pkg_dir = pathlib.Path(TEST_ROOT).joinpath(pkg_name)
+    pkg_dir.mkdir(exist_ok=True)
+    init_path = pkg_dir.joinpath("__init__.py")
+    with init_path.open(mode="w") as f_out:
+        f_out.write(f"# {pkg_name}/__init__ file")
 
     for name, content in pkg_content.items():
-        module_path = pathlib.Path(f"{pkg_name}/{name}")
-        with open(module_path) as f_out:
+        module_path = pkg_dir.joinpath(name)
+        with module_path.open(mode="w") as f_out:
             f_out.write(content)
     return True
 
