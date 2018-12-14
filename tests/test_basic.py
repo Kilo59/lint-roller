7bd0ce39bf80c248a036b59f7e33afdf2a794e74
Gabriel <gabriel59kg@gmail.com>
2018-12-14 09:41:34 -0500

    use tmpdir

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
 import pytest
+import lint_roller as lr
 
 
-def test_for_fire():
-    """Everything is on fire."""
-    import lint_roller
+def test_basic_pkg_gen(tmpdir):
+    """Ensure no errors when generating a package using defaults.
+    Excluding custom package default
+    TODO: parametrize with tmpdir and default dir"""
+    assert isinstance(
+        lr.utils.package_maker("my_test_pkg", new_pkg_dir_path=tmpdir), Path
+    )
---
+++ tests/test_smoke.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+
+def test_for_fire():
+    """Everything is on fire."""
+    import lint_roller
