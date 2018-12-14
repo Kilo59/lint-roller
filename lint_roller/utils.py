8ae4f9b8e625d2dfb3006f95905b48a58b5b0f04
Gabriel <gabriel59kg@gmail.com>
2018-12-14 16:50:53 -0500

    more error checking

--- lint_roller/utils.py
+++ lint_roller/utils.py
@@ -7,7 +7,7 @@ lint-roller utility methods
 import pathlib
 import shutil
 from typing import Dict, Optional
-import pytest
+from py._path.local import LocalPath
 
 PKG_ROOT = pathlib.Path(__file__).joinpath("..").resolve()
 ROOT = PKG_ROOT.joinpath("..").resolve()
@@ -18,7 +18,7 @@ DATA = ROOT.joinpath("data")
 def package_maker(
     pkg_name: str,
     pkg_content: Optional[Dict[str, str]] = None,
-    new_pkg_dir_path: Optional[pathlib.Path] = None,
+    new_pkg_path: Optional[pathlib.Path] = None,
 ) -> pathlib.Path:
     """Create the directory structure and source code for a Python package.
 
@@ -29,7 +29,7 @@ def package_maker(
     pkg_content
         Dictionary of package module names and string of module source code as value
         (the default is None, which will use some boilerplate code in need of linting)
-    new_pkg_dir_path
+    new_pkg_path
         Where to create the new package (the default is None, which will store it in a
         sibling directory called `pkg_depot/`)
 
@@ -45,19 +45,21 @@ def package_maker(
     elif not isinstance(pkg_content, dict):
         raise TypeError("pkg_content should be a dictionary")
 
-    if not new_pkg_dir_path:
+    if not new_pkg_path:
         DEPOT.mkdir(exist_ok=True)
-        new_pkg_dir_path = pathlib.Path(DEPOT).joinpath(pkg_name)
+        new_pkg_path = pathlib.Path(DEPOT).joinpath(pkg_name)
+    elif isinstance(new_pkg_path, (pathlib.Path, str, LocalPath)):
+        new_pkg_path = pathlib.Path(new_pkg_path).resolve()
     else:
-        new_pkg_dir_path = pathlib.Path(new_pkg_dir_path).resolve()
+        raise TypeError("new_pkg_path should be Path object or path string")
 
-    new_pkg_dir_path.mkdir(exist_ok=True)
-    init_path = new_pkg_dir_path.joinpath("__init__.py")
+    new_pkg_path.mkdir(exist_ok=True)
+    init_path = new_pkg_path.joinpath("__init__.py")
     with init_path.open(mode="w") as f_out:
         f_out.write(f"# {pkg_name}/__init__ file")
 
     for name, content in pkg_content.items():
-        module_path = new_pkg_dir_path.joinpath(name)
+        module_path = new_pkg_path.joinpath(name)
         with module_path.open(mode="w") as f_out:
             f_out.write(content)
-    return new_pkg_dir_path
+    return new_pkg_path
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -8,13 +8,13 @@ def test_basic_pkg_gen(tmpdir):
     """Ensure no errors when generating a package using defaults.
     Excluding custom package default
     TODO: parametrize with tmpdir and default dir"""
-    assert isinstance(
-        lr.utils.package_maker("my_test_pkg", new_pkg_dir_path=tmpdir), Path
-    )
+    print(type(tmpdir))
+    assert isinstance(lr.utils.package_maker("my_test_pkg", new_pkg_path=tmpdir), Path)
 
 
 @pytest.mark.parametrize(
-    "name,content,pkg_dir", [("bad_content_pkg", "not_a_dict", None)]
+    "name,content,pkg_dir",
+    [("bad_content_pkg", "not_a_dict", None), ("bad_path_pkg", None, 123)],
 )
 def test_basic_pkg_gen_bad_args(name, content, pkg_dir):
     with pytest.raises(TypeError):
